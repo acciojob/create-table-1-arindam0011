c22cf02ee82226f4777fb253e650a25ee094a434
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-06-12 00:43:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function insert_Row() {
     //Write your code here
   let row= document.createElement("tr");
 	row.innerHTML=`
-					<td>New Cell1</td>
+					<td>New Cell1Row1 cell1</td>
 					<td>New Cell2</td>
 					`;
 	table.prepend(row);
