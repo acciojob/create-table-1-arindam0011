3446a803eeefcdd6c9a0efdc8aaa6324091ea24d
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-06-12 00:25:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function insert_Row() {
+	let table= document.getElementById("sampleTable");
     //Write your code here
-  
+  let row= document.createElement("tr");
+	row.innerHTML=`
+					<td>Row${table.rows.length+1} cell1</td>
+					<td>Row${table.rows.length+1} cell2</td>
+					`;
+	table.appendChild(row);
   
 }
